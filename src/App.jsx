00a6b430e6d1a3eebc0b17a9a3fe02ea2e944020
Alsu <mukhamatkhanovaalsu@gmail.com>
2023-08-05 13:40:15 +0600

    Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import Counter from './components/Counter/Counter';
 
 function App() {
   return (
     <div>
       <div className='wrapper'>
         <Header />
+        <main>
+          <Counter />
+        </main>
       </div>
     </div>
   );
