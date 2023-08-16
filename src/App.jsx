8e95a0110760bf3e55473a77a48e79fcfdb0d185
Alsu <mukhamatkhanovaalsu@gmail.com>
2023-08-16 13:34:57 +0600

    axios_and_api

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Counter from './components/Counter/Counter';
 import Modal from './components/Modal/Modal';
 import Quiz from './components/Quiz/Quiz';
 import Result from './components/Quiz/Result';
+import axios from 'axios'
 
-const questions = [
-  {
-    title: 'Сколько лапок у паука?',
-    variants: ['8', '4', '40'],
-    correct: 0,
-  },
-  {
-    title: 'Зимой и летом одним цветом ',
-    variants: ['светофор', 'елочка', 'что угодно'],
-    correct: 1,
-  },
-  {
-    title: 'Сколько часов в сутки человеку нужно спать?',
-    variants: ['весь день','всю ночь','8 часов'],
-    correct: 2,
-  },
-];
+// const questions = [
+//   {
+//     title: 'Сколько лапок у паука?',
+//     variants: ['8', '4', '40'],
+//     correct: 0,
+//   },
+//   {
+//     title: 'Зимой и летом одним цветом ',
+//     variants: ['светофор', 'елочка', 'что угодно'],
+//     correct: 1,
+//   },
+//   {
+//     title: 'Сколько часов в сутки человеку нужно спать?',
+//     variants: ['весь день','всю ночь','8 часов'],
+//     correct: 2,
+//   },
+// ];
+
+const baseURL = "https://64db776d593f57e435b10048.mockapi.io/questions";
 
 function App() {
 
   const [step, setStep] = useState(0)
-  const question = questions[step]
   const [correct, setCorrect] = useState(0)
 
+  const [questions, setQuestions] = useState([]);
+  const question = questions[step]
+  
+  
+useEffect(() => {
+  axios.get(baseURL).then((response) => {
+    setQuestions(response.data);
+  });
+}, []);
+
   const onClickVariant = (index) => {
-    console.log(index)
     setStep(step + 1)
 
     if (index === question.correct) {
@@ -48,7 +59,7 @@ function App() {
       <main>
         <Counter />
         <Modal />
-        { prosent != 100 ? <Quiz prosent = {prosent} question = {question} step ={step} onClickVariant= {onClickVariant} /> : <Result correct={correct} questions={questions}/> }
+        { prosent != 100 ? <Quiz question={question} prosent = {prosent}  step ={step} onClickVariant= {onClickVariant} /> : <Result correct={correct} questions={questions}/> }
       </main>
     </div>
   </div>
